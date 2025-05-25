d46c3eada7c46085958e2eae35d48020452920de
T-REX911 <163975890+T-REX911@users.noreply.github.com>
2025-05-25 21:58:56 +0530

    Initial Commit

---
+++ SpringSecurity_03/src/test/java/com/spring/SpringSecurity_03/SpringSecurity_03/SpringSecurity03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.SpringSecurity_03.SpringSecurity_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
