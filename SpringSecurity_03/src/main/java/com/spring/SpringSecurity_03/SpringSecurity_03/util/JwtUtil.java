483338ef4dd4d74fa7fd320f378815156d1994fc
T-REX911 <163975890+T-REX911@users.noreply.github.com>
2025-05-25 22:10:27 +0530

    Add JwtUtil Class

---
+++ SpringSecurity_03/src/main/java/com/spring/SpringSecurity_03/SpringSecurity_03/util/JwtUtil.java
@@ -0,0 +1,42 @@
+package com.spring.SpringSecurity_03.SpringSecurity_03.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET = "secret_key_for_jwt_should_be_secure";
+
+    public String generateToken(String username, List<String> roles) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public List<String> extractRoles(String token) {
+        return extractClaims(token).get("roles", List.class);
+    }
+}
